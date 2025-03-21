aa7a5d4f1ddffa1fa88208a6a4ab6ef9caea9e86
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2025-03-20 17:06:23 -0700

    ProyectoCERO
    estan las 3 capas y el modulo InscribirClase

---
+++ ProyectoJavaCERO/CERO/Persistencia/src/main/java/com/mycompany/persistencia/Persistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.persistencia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Persistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
